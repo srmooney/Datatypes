7344a08522550ed7c6e8bdd3e316411e6b51b458
WhiteSpace-Creative <sean@whitespace-creative.com>
2015-01-13 09:09:57 -0500

    First Commit
    Umbraco Datatypes

---
+++ WSC.DataType.Children.vb
@@ -0,0 +1,173 @@
+﻿Imports Microsoft.VisualBasic
+Imports umbraco.interfaces
+Imports umbraco
+Imports System.Web.UI.WebControls
+Imports System.Web.UI
+
+Namespace WSC.DataType.Children
+        Public Class DataType
+        Inherits umbraco.cms.businesslogic.datatype.AbstractDataEditor
+
+            Private _Editor As IDataEditor
+            Private _baseData As IData
+            Private _prevalueeditor As IDataPrevalue
+
+            Public Overrides ReadOnly Property Data As IData
+                Get
+                    If _baseData Is Nothing Then
+                        _baseData = New cms.businesslogic.datatype.DefaultData(Me)
+                    End If
+                    Return _baseData
+                End Get
+            End Property
+
+            Public Overrides ReadOnly Property DataEditor As IDataEditor
+                Get
+                    If _Editor Is Nothing Then
+                        _Editor = New DataEditor(Data)
+                    End If
+                    Return _Editor
+                End Get
+            End Property
+
+            Public Overrides ReadOnly Property DataTypeName As String
+                Get
+                Return "Children as list"
+                End Get
+            End Property
+
+            Public Overrides ReadOnly Property Id As Guid
+                Get
+                Return New Guid("02fe6ae2-47a5-44f5-92f7-cd29a5b6329a")
+                End Get
+            End Property
+
+            Public Overrides ReadOnly Property PrevalueEditor As IDataPrevalue
+                Get
+                    If _prevalueeditor Is Nothing Then
+                        _prevalueeditor = New editorControls.DefaultPrevalueEditor(Me, False)
+                    End If
+                    Return _prevalueeditor
+                End Get
+            End Property
+
+        End Class
+
+    <ClientDependency.Core.ClientDependency(ClientDependency.Core.ClientDependencyType.Css, "//cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.17.0/css/theme.ice.css")>
+    <ClientDependency.Core.ClientDependency(ClientDependency.Core.ClientDependencyType.Javascript, "//cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.17.0/jquery.tablesorter.min.js")>
+    Public Class DataEditor
+        Inherits PlaceHolder
+        Implements IDataEditor
+
+        Public ReadOnly Property Editor As Control Implements IDataEditor.Editor
+            Get
+                Return Me
+            End Get
+        End Property
+
+        Public ReadOnly Property ShowLabel As Boolean Implements IDataEditor.ShowLabel
+            Get
+                Return False
+            End Get
+        End Property
+
+        Public ReadOnly Property TreatAsRichTextEditor As Boolean Implements IDataEditor.TreatAsRichTextEditor
+            Get
+                Return False
+            End Get
+        End Property
+
+        Public Sub New(data As interfaces.IData)
+        End Sub
+
+        Protected Overrides Sub OnInit(e As EventArgs)
+            MyBase.OnInit(e)
+        End Sub
+
+        Protected Overrides Sub OnLoad(e As EventArgs)
+            MyBase.OnLoad(e)
+            Dim u = umbraco.helper.GetCurrentUmbracoUser()
+
+            Dim d = Nothing
+            If Not Page.IsPostBack Then
+                Dim sb As New StringBuilder
+                Dim parentID = MyBase.Page.Request("id")
+                Dim p = umbraco.Core.ApplicationContext.Current.Services.ContentService.GetById(parentID)
+                Dim children = umbraco.Core.ApplicationContext.Current.Services.ContentService.GetChildren(parentID)
+
+                If children.Count > 0 Then
+                    sb.AppendFormat("<table class=""tablesorter tablesorter-ice"" id=""{0}"">", Me.ClientID)
+                    sb.Append("<thead>")
+                    sb.Append("<tr>")
+                    sb.Append("<th>Name</th>")
+                    sb.Append("<th>Create Date</th>")
+                    sb.Append("<th>Last Update</th>")
+                    sb.Append("<th>Published</th>")
+                    sb.Append("<th>")
+                    If (u.GetPermissions(p.Path).Contains("C")) Then
+                        sb.Append("<a href=""#"" class=""add"">Add New</a>")
+                    End If
+                    sb.Append("</th>")
+                    sb.Append("</tr>")
+                    sb.Append("</thead>")
+                    sb.Append("<tbody>")
+
+                    For Each c In children
+                        sb.Append("<tr>")
+                        sb.AppendFormat("<td><a href=""/umbraco/editContent.aspx?id={1}"">{0}</a></td>", c.Name, c.Id)
+                        sb.AppendFormat("<td>{0}</td>", c.CreateDate)
+                        sb.AppendFormat("<td>{0}</td>", c.UpdateDate)
+                        sb.AppendFormat("<td><input type=""checkbox"" disabled=""disabled"" {0}/></td>", If(c.Published, "checked=""checked""", String.Empty))
+                        If (u.GetPermissions(c.Path).Contains("D")) Then
+                            sb.AppendFormat("<td><a href=""#"" class=""delete"" data-id=""{0}"">delete</a></td>", c.Id)
+                        End If
+                        sb.Append("</tr>")
+                    Next
+                    sb.Append("</tbody>")
+                    sb.Append("</table>")
+                    sb.AppendLine("<script>")
+                    sb.AppendLine("$(function(){")
+                    sb.AppendLine(" var p = UmbClientMgr.mainTree().findNode(" & parentID & ", false);")
+                    sb.AppendLine(" $('#" & Me.ClientID & "').tablesorter({")
+                    sb.AppendLine("      sortList:[[0,0]]")
+                    sb.AppendLine("     ,headers: { 4:{sorter:false} }")
+                    sb.AppendLine("     ,textExtraction:{3:function(node, table, cellIndex){ return $(node).find('input').is(':checked'); }}")
+                    sb.AppendLine(" }).on('click', 'a.delete', function(e){ e.preventDefault(); var id = $(this).data('id');")
+                    sb.AppendLine("     var tr = $(this).closest('tr');")
+                    sb.AppendLine("     UmbClientMgr.mainTree().onBeforeContext(p);")
+                    sb.AppendLine("     UmbClientMgr.mainTree()._loadChildNodes(UmbClientMgr.mainTree().getActionNode().jsNode, function(){")
+                    sb.AppendLine("         var n = UmbClientMgr.mainTree().findNode(id, false);")
+                    sb.AppendLine("         if (n){")
+                    sb.AppendLine("             UmbClientMgr.mainTree().onBeforeContext(n);")
+                    sb.AppendLine("             n = UmbClientMgr.mainTree().getActionNode();")
+                    sb.AppendLine("             if (confirm(UmbClientMgr.uiKeys()['defaultdialogs_confirmdelete'] + ' ""' + n.nodeName + '""?\n\n')) {")
+                    sb.AppendLine("                 jQuery(window.top).trigger('nodeDeleting', []);")
+                    sb.AppendLine("                 top.umbraco.presentation.webservices.legacyAjaxCalls.Delete(")
+                    sb.AppendLine("                      n.nodeId")
+                    sb.AppendLine("                     ,n.nodeName")
+                    sb.AppendLine("                     ,n.nodeType")
+                    sb.AppendLine("                     ,function(){jQuery(window.top).trigger('nodeDeleted', []); UmbClientMgr.mainTree().onBeforeContext(p); UmbClientMgr.mainTree()._loadChildNodes(UmbClientMgr.mainTree().getActionNode().jsNode, null); tr.remove();}")
+                    sb.AppendLine("                     ,function(error) { jQuery(window.top).trigger('publicError', [error]); }")
+                    sb.AppendLine("                 )")
+                    sb.AppendLine("             }") '--End If(confirm)
+                    sb.AppendLine("         }") '--End If(n)
+                    sb.AppendLine("     });")
+                    sb.AppendLine(" }).on('click', 'a.add', function(e){ e.preventDefault();")
+                    sb.AppendLine("     UmbClientMgr.mainTree().onBeforeContext(p);")
+                    sb.AppendLine("     top.UmbClientMgr.appActions().actionNew();")
+                    sb.AppendLine(" })")
+                    sb.AppendLine("});")
+                    sb.AppendLine("</script>")
+                End If
+
+                Me.Controls.Add(New LiteralControl(sb.ToString))
+            End If
+        End Sub
+
+        Public Sub Save() Implements IDataEditor.Save
+        End Sub
+    End Class
+
+End Namespace
+
+
---
+++ WSD.Datatype.Typeahead.vb
@@ -0,0 +1,328 @@
+﻿Imports Microsoft.VisualBasic
+Imports umbraco.interfaces
+Imports umbraco
+Imports System.Web.UI.WebControls
+Imports System.Web.UI
+Imports umbraco.editorControls.PrevalueEditorExtensions
+
+Namespace WSC.DataType
+    Namespace Autocomplete
+
+        <umbraco.Web.BaseRest.RestExtension(DataType.Identifier)>
+        Public Class Base
+            Private Shared Function GetOptions(datatypeDefinitionId As Integer) As Options
+                Return DirectCast(HttpContext.Current.Cache(String.Concat(DataType.Identifier, "_options_", datatypeDefinitionId)), Options)
+            End Function
+
+            <umbraco.Web.BaseRest.RestExtensionMethod(ReturnXML:=False)>
+            Public Shared Function GetData(datatypeDefinitionID As Integer) As String
+                Dim autocompleteText = HttpContext.Current.Request("autocompleteText")
+                Dim ret As New List(Of String)
+
+                Dim options = GetOptions(datatypeDefinitionID)
+
+                If (options IsNot Nothing AndAlso autocompleteText.Length >= options.MinLength) Then
+                    If options.SQL.Contains("%@autoCompleteText%") Then
+                        options.SQL = options.SQL.Replace("'%@autoCompleteText%'", "@0")
+                        autocompleteText = "%" & autocompleteText & "%"
+                    End If
+                    Dim db = umbraco.Core.ApplicationContext.Current.DatabaseContext.Database
+                    Dim strSQL = Core.Persistence.Sql.Builder.Append(options.SQL, autocompleteText)
+
+                    ret = db.Fetch(Of String)(strSQL)
+                    'ret.Add(db.LastCommand)
+                End If
+
+                Return Newtonsoft.Json.JsonConvert.SerializeObject(ret.ToArray).ToString
+            End Function
+
+        End Class
+
+        Public Class Options
+            Inherits editorControls.AbstractOptions
+
+            Public Sub New()
+                MyBase.New()
+            End Sub
+
+            Public Sub New(loadDefaults As Boolean)
+                MyBase.New(loadDefaults)
+            End Sub
+
+            <ComponentModel.DefaultValue("")>
+            Public Property SQL() As String
+
+            <ComponentModel.DefaultValue(3)>
+            Public Property MinLength() As Integer
+        End Class
+
+        Public Class PrevalEditor
+            Inherits editorControls.AbstractJsonPrevalueEditor
+
+            Private txtSQL As New TextBox()
+            Private ddlMinLength As New DropDownList()
+
+            Private m_Options As Options
+            ''' <summary>
+            ''' Gets the options data object that represents the current state of this datatypes configuration
+            ''' </summary>
+            Friend ReadOnly Property Options() As Options
+                Get
+                    If Me.m_Options Is Nothing Then
+                        '--Deserialize any stored settings for this PreValueEditor instance
+                        Me.m_Options = Me.GetPreValueOptions(Of Options)()
+                        '--If still null, ie, object couldn't be de-serialized from PreValue[0] string value
+                        If Me.m_Options Is Nothing Then
+                            '--Create a new Options data object with the default values
+                            Me.m_Options = New Options(True)
+                        End If
+                    End If
+
+                    Return Me.m_Options
+                End Get
+            End Property
+
+            Public Sub New(dataType As umbraco.cms.businesslogic.datatype.BaseDataType)
+                MyBase.New(dataType)
+            End Sub
+
+            Public Overrides Sub Save()
+                '--Set the database data-type
+                Me.m_DataType.DBType = cms.businesslogic.datatype.DBTypes.Nvarchar
+                '--Set the options
+                Dim options = New Options() With {.MinLength = ddlMinLength.SelectedValue, .SQL = txtSQL.Text}
+                '--Save the options as JSON
+                Me.SaveAsJson(options)
+                '--Cache settings
+                HttpContext.Current.Cache(String.Concat(DataType.Identifier, "_options_", Me.m_DataType.DataTypeDefinitionId)) = options
+            End Sub
+
+            Protected Overrides Sub OnInit(e As EventArgs)
+                MyBase.OnInit(e)
+                Me.EnsureChildControls()
+            End Sub
+
+            Protected Overrides Sub CreateChildControls()
+                MyBase.CreateChildControls()
+
+                Me.txtSQL.TextMode = TextBoxMode.MultiLine
+                Me.txtSQL.ID = "txtSQL"
+                Me.txtSQL.Columns = 60
+                Me.txtSQL.Rows = 10
+
+                Me.ddlMinLength.ID = "ddlMinLength"
+                Me.ddlMinLength.Items.Add(New ListItem("1"))
+                Me.ddlMinLength.Items.Add(New ListItem("2"))
+                Me.ddlMinLength.Items.Add(New ListItem("3"))
+                Me.ddlMinLength.Items.Add(New ListItem("4"))
+                Me.ddlMinLength.Items.Add(New ListItem("5"))
+
+                Me.Controls.AddPrevalueControls(Me.txtSQL, Me.ddlMinLength)
+            End Sub
+
+            Protected Overrides Sub OnLoad(e As EventArgs)
+                MyBase.OnLoad(e)
+                Me.txtSQL.Text = Me.Options.SQL
+                Try
+                    Me.ddlMinLength.SelectedValue = Me.Options.MinLength
+                Catch ex As Exception
+                End Try
+            End Sub
+
+            Protected Overrides Sub RenderContents(writer As HtmlTextWriter)
+                writer.AddPrevalueRow("ID", New LiteralControl(Me.m_DataType.DataTypeDefinitionId))
+                writer.AddPrevalueRow("Min Length", "number of chars in the autocomplete text box before querying for data", Me.ddlMinLength)
+                writer.AddPrevalueRow("SQL Expression:", "expects a result set with one fields : 'Text' - can include the token : @autoCompleteText", Me.txtSQL)
+                writer.AddPrevalueRow("Post Url", "post autoCompleteText variable to the above url", New LiteralControl("/Base/" & DataType.Identifier & "/GetData/" & Me.m_DataType.DataTypeDefinitionId & "/"))
+
+            End Sub
+        End Class
+
+        <ClientDependency.Core.ClientDependency(ClientDependency.Core.ClientDependencyType.Javascript, "ui/jqueryui.js", "UmbracoClient")>
+        <ValidationProperty("IsValid")>
+        Public Class DataEditor
+            Inherits CompositeControl
+            Implements IDataEditor
+
+            Private data As IData
+            Private opts As Options
+            Private txtValue As New TextBox()
+
+            ''' <summary>
+            ''' Initializes a new instance of SqlAutoCompleteDataEditor
+            ''' </summary>
+            ''' <param name="data"></param>
+            ''' <param name="options"></param>
+            Friend Sub New(data As IData, options As Options)
+                Me.data = data
+                Me.opts = options
+            End Sub
+
+            ''' <summary>
+            ''' Gets the property / datatypedefinition id - used to identify the current instance
+            ''' </summary>
+            Private ReadOnly Property DataTypeDefinitionId() As Integer
+                Get
+                    Return DirectCast(Me.data, editorControls.XmlData).DataTypeDefinitionId
+                End Get
+            End Property
+
+
+            Public ReadOnly Property Editor As Control Implements IDataEditor.Editor
+                Get
+                    Return Me
+                End Get
+            End Property
+
+            Public ReadOnly Property ShowLabel As Boolean Implements IDataEditor.ShowLabel
+                Get
+                    Return True
+                End Get
+            End Property
+
+            Public ReadOnly Property TreatAsRichTextEditor As Boolean Implements IDataEditor.TreatAsRichTextEditor
+                Get
+                    Return False
+                End Get
+            End Property
+
+            Protected Overrides Sub CreateChildControls()
+                MyBase.CreateChildControls()
+
+                Me.txtValue.ID = "txtValue"
+                Me.txtValue.CssClass = "umbEditorTextField"
+                Me.Controls.Add(Me.txtValue)
+            End Sub
+
+            Protected Overrides Sub OnLoad(e As EventArgs)
+                MyBase.OnLoad(e)
+                Me.EnsureChildControls()
+
+                Dim startupScript As New StringBuilder()
+                startupScript.Append("$(function(){")
+                startupScript.AppendFormat("$('#{0}').each(function(){{", Me.txtValue.ClientID)
+                startupScript.Append("var input = $(this);")
+                startupScript.Append("input.autocomplete({")
+                startupScript.AppendFormat("    minLength: {0},", Me.opts.MinLength)
+                startupScript.Append("    source: function (request, response) {")
+                startupScript.Append("        jQuery.ajax({")
+                startupScript.Append("            type: 'POST',")
+                startupScript.Append("            data: { autoCompleteText: request.term },")
+                startupScript.Append("            contentType: 'application/x-www-form-urlencoded; charset=utf-8',")
+                startupScript.AppendFormat("            url: '/Base/{0}/GetData/{1}/',", DataType.Identifier, Me.DataTypeDefinitionId)
+                startupScript.Append("            dataType: 'json',")
+                startupScript.Append("            success: response")
+                startupScript.Append("        });")
+                startupScript.Append("    },")
+                startupScript.Append("    open: function (event, ui) {")
+                startupScript.Append("        input.autocomplete('widget').width(300); /* TODO: can we get at the input field from the event or ui params ? */")
+                startupScript.Append("    },")
+                startupScript.Append("    autoFocus: true,")
+                startupScript.Append("    create: function (event, ui) {")
+                startupScript.Append("        input.autocomplete('widget').addClass('sql-auto-complete-widget');")
+                startupScript.Append("    }")
+                startupScript.Append("});")
+                startupScript.Append("});")
+                startupScript.Append("});")
+
+                Dim css As New StringBuilder()
+                css.Append("<style>")
+                css.Append(".ui-menu { list-style: none; /*padding: 2px; margin: 0;*/ }")
+                css.Append("</style>")
+
+                ScriptManager.RegisterStartupScript(Me, GetType(DataEditor), Me.ClientID & "_init", startupScript.ToString, True)
+                ScriptManager.RegisterStartupScript(Me, GetType(DataEditor), Me.ClientID & "_css", css.ToString, False)
+
+                If Not Me.Page.IsPostBack Then
+                    Me.txtValue.Text = Me.data.Value.ToString
+                End If
+
+                '--Put the options obj into cache so that the /base method can request it (where the sql statment is being used)
+                HttpContext.Current.Cache(String.Concat(DataType.Identifier, "_options_", Me.DataTypeDefinitionId)) = Me.opts
+            End Sub
+
+            Public Sub Save() Implements IDataEditor.Save
+                Me.data.Value = txtValue.Text
+            End Sub
+
+            Public ReadOnly Property IsValid As String
+                Get
+                    Return Me.txtValue.Text
+                End Get
+            End Property
+
+
+        End Class
+
+        Public Class DataType
+            Inherits umbraco.cms.businesslogic.datatype.BaseDataType
+            Implements IDataType
+
+            Public Const Identifier As String = "9cd0dc2e-8c62-401a-8114-964707b07f4f"
+
+            Private m_PrevalEditor As Autocomplete.PrevalEditor
+            Private m_DataEditor As Autocomplete.DataEditor
+            Private m_Data As IData
+            Private m_Options As Autocomplete.Options
+
+            Private ReadOnly Property Options As Options
+                Get
+                    If Me.m_Options Is Nothing Then
+                        Me.m_Options = DirectCast(Me.PrevalueEditor, PrevalEditor).Options
+                    End If
+                    Return Me.m_Options
+                End Get
+            End Property
+
+            Public Overrides ReadOnly Property DataTypeName As String Implements IDataType.DataTypeName
+                Get
+                    Return "Autocomplete"
+                End Get
+            End Property
+
+            Public Overrides ReadOnly Property Id As Guid Implements IDataType.Id
+                Get
+                    Return New Guid(Identifier)
+                End Get
+            End Property
+
+            Public Overrides ReadOnly Property PrevalueEditor As IDataPrevalue Implements IDataType.PrevalueEditor
+                Get
+                    If (Me.m_PrevalEditor Is Nothing) Then
+                        Me.m_PrevalEditor = New PrevalEditor(Me)
+                    End If
+
+                    Return Me.m_PrevalEditor
+                End Get
+            End Property
+
+            Public Overrides ReadOnly Property DataEditor As IDataEditor Implements IDataType.DataEditor
+                Get
+                    If (Me.m_DataEditor Is Nothing) Then
+                        Me.m_DataEditor = New DataEditor(Me.Data, Me.Options)
+                    End If
+
+                    Return Me.m_DataEditor
+                End Get
+            End Property
+
+            Public Overrides ReadOnly Property Data As IData Implements IDataType.Data
+                Get
+                    If Me.m_Data Is Nothing Then
+                        Me.m_Data = New editorControls.XmlData(Me)
+                    End If
+                    Return Me.m_Data
+                End Get
+            End Property
+
+            Public Overloads Property DataTypeDefinitionId As Integer Implements IDataType.DataTypeDefinitionId
+                Get
+                    Return MyBase.DataTypeDefinitionId
+                End Get
+                Set(value As Integer)
+                    MyBase.DataTypeDefinitionId = value
+                End Set
+            End Property
+        End Class
+    End Namespace
+End Namespace
